90fb4c27d000df99e9a7c3adaad376796c45208a
Ary <aryarte>
2023-05-23 20:41:27 +0800

    make dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -3,35 +3,45 @@
             <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
                 <div class="d-flex">
                     <div>
-                        <div class="p-2 px-2 text-light bg-primary rounded-pill">
-                            $
+                        <div class="p-2 px-2 text-light bg-primary rounded">
+                            <i class="bi bi-t-shirt fs-2"></i>
                         </div>
                     </div>
                     <div class="d-flex flex-column justify-content-between">
                         <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
                         <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
                         <h1 style="font-size: 40px">10K</h1>
-                        <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>                    
+                    </div>                    
                     </div>
                 </div>
             </div>
             <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
                 <div class="d-flex">
+                    <div>
+                        <div class="p-2 px-2 text-light bg-primary rounded">
+                            <i class="bi bi-bag-fill fs-2"></i>
+                        </div>
+                    </div>
                     <div class="d-flex flex-column justify-content-between">
                         <p class="text-left px-2 mb-0 fs-6 text-secondary">Orders</p>
                         <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
                         <h1 style="font-size: 40px">10K</h1>
-                        <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>                    
+                    </div>                    
                     </div>
                 </div>
             </div>
             <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
                 <div class="d-flex">
+                    <div>
+                        <div class="p-2 px-2 text-light bg-primary rounded">
+                            <i class="bi bi-person-fill fs-2"></i>
+                        </div>
+                    </div>
                     <div class="d-flex flex-column justify-content-between">
                         <p class="text-left px-2 mb-0 fs-6 text-secondary">Admin</p>
                         <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
                         <h1 style="font-size: 40px">10K</h1>
-                        <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>                    
+                    </div>                    
                     </div>
                 </div>
             </div>            
