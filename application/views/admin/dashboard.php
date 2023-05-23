6febe88e8572515b91b6d8fa1623383cd7a08fc4
Ary <aryarte>
2023-05-23 20:32:04 +0800

    make dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,30 +1,38 @@
     <div class="col-8">        
         <div class="mt-5 d-flex flex-wrap justify-content-center bg-light" style="width: 100%;">            
             <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
-                <div class="d-flex flex-column justify-content-between">
-                    <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
-                    <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
-                    <h1 style="font-size: 40px">10K</h1>
-                <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>
-                    
+                <div class="d-flex">
+                    <div>
+                        <div class="p-2 px-2 text-light bg-primary rounded-pill">
+                            $
+                        </div>
+                    </div>
+                    <div class="d-flex flex-column justify-content-between">
+                        <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
+                        <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
+                        <h1 style="font-size: 40px">10K</h1>
+                        <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>                    
+                    </div>
                 </div>
             </div>
             <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
-                <div class="d-flex flex-column justify-content-between">
-                    <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
-                    <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
-                    <h1 style="font-size: 40px">10K</h1>
-                <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>
-                    
+                <div class="d-flex">
+                    <div class="d-flex flex-column justify-content-between">
+                        <p class="text-left px-2 mb-0 fs-6 text-secondary">Orders</p>
+                        <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
+                        <h1 style="font-size: 40px">10K</h1>
+                        <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>                    
+                    </div>
                 </div>
             </div>
             <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
-                <div class="d-flex flex-column justify-content-between">
-                    <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
-                    <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
-                    <h1 style="font-size: 40px">10K</h1>
-                <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>
-                    
+                <div class="d-flex">
+                    <div class="d-flex flex-column justify-content-between">
+                        <p class="text-left px-2 mb-0 fs-6 text-secondary">Admin</p>
+                        <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
+                        <h1 style="font-size: 40px">10K</h1>
+                        <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>                    
+                    </div>
                 </div>
             </div>            
         </div>
