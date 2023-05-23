5902b5b2c70919210d42e773c718915fc6c09f89
Ary <aryarte>
2023-05-23 20:23:33 +0800

    make dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -1,35 +1,33 @@
-    <div class="col-8">
-        <div class="containner mt-5 d-flex flex-wrap">
-            <div class="card bg-primary py-2 p-2 mt-2" style="width: 16rem;">
+    <div class="col-8">        
+        <div class="mt-5 d-flex flex-wrap justify-content-center bg-light" style="width: 100%;">            
+            <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
                 <div class="d-flex flex-column justify-content-between">
-                    <h1 class="text-center text-light">Products</h1>
-                    <div class="p-2 rounded align-middle bg-abu-abu text-dark fs-5 text-center align-middle" style="width: auto">1000000</div>
+                    <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
+                    <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
+                    <h1 style="font-size: 40px">10K</h1>
+                <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>
+                    
                 </div>
             </div>
-            <div class="card bg-primary py-2 p-2 mt-2" style="width: 16rem;">
+            <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
                 <div class="d-flex flex-column justify-content-between">
-                    <h1 class="text-center text-light">Products</h1>
-                    <div class="p-2 rounded align-middle bg-abu-abu text-dark fs-5 text-center align-middle" style="width: auto">1000000</div>
+                    <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
+                    <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
+                    <h1 style="font-size: 40px">10K</h1>
+                <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>
+                    
                 </div>
             </div>
-            <div class="card bg-primary py-2 p-2 mt-2" style="width: 16rem;">
+            <div class="bg-light py-2 p-2 mt-2" style="width: 16rem;">
                 <div class="d-flex flex-column justify-content-between">
-                    <h1 class="text-center text-light">Products</h1>
-                    <div class="p-2 rounded align-middle bg-abu-abu text-dark fs-5 text-center align-middle" style="width: auto">1000000</div>
+                    <p class="text-left px-2 mb-0 fs-6 text-secondary">Products</p>
+                    <div class="px-2 rounded align-middle text-dark text-left align-middle" style="width: auto; height: 150px;">
+                    <h1 style="font-size: 40px">10K</h1>
+                <p class="text-left mb-0 text-secondary" style="font-size: 12px">12 Desembar 2022</p></div>
+                    
                 </div>
-            </div>
-            <div class="card bg-primary py-2 p-2 mt-2" style="width: 16rem;">
-                <div class="d-flex flex-column justify-content-between">
-                    <h1 class="text-center text-light">Products</h1>
-                    <div class="p-2 rounded align-middle bg-abu-abu text-dark fs-5 text-center align-middle" style="width: auto">1000000</div>
-                </div>
-            </div>
-            <div class="card bg-primary py-2 p-2 mt-2" style="width: 16rem;">
-                <div class="d-flex flex-column justify-content-between">
-                    <h1 class="text-center text-light">Products</h1>
-                    <div class="p-2 rounded align-middle bg-abu-abu text-dark fs-5 text-center align-middle" style="width: auto">1000000</div>
-                </div>
-            </div>
+            </div>            
         </div>
+        
     </div>
 </div>
